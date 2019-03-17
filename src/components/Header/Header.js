237b40071746f77d613398cfbfb35e7d527f6d3d
Mark Romero <markromero@marks-mbp.lan>
2019-03-17 00:33:09 -0600

    completed menu, need to add animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Header.css';
 
-export default function Header() {
-  return (
-    <nav className='navbar'>
-      <div className='nav navbar-logo'>
-        Start BootStrap
-      </div>
-      <ul className='nav nav-items nav-right'>
-        <li className='navbar-item'><a href="#" className="navbar-link">Services</a></li>
-        <li className='navbar-item'><a href="#" className="navbar-link">Portfolio</a></li>
-        <li className='navbar-item'><a href="#" className="navbar-link">About</a></li>
-        <li className='navbar-item'><a href="#" className="navbar-link">Team</a></li>
-        <li className='navbar-item'><a href="#" className="navbar-link">Contact</a></li>
-      </ul>
-    </nav>
-  )
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showMenu: true
+    }
+  }
+
+  toggleMenu = () => {
+    const menuStatus = this.state.showMenu ? false : true;
+
+    this.setState({
+      showMenu: menuStatus
+    })
+  }
+  render () {
+    return (
+      <nav className='navbar'>
+        <div className='nav navbar-logo navbar-logo-section'>
+          Start BootStrap
+          <button className='btn nav-btn' onClick={this.toggleMenu}>
+            <span className='btn-text'>Menu</span>
+            <i className="fas fa-bars"></i> 
+          </button>
+        </div>
+        <ul className={`nav nav-items nav-right ${this.state.showMenu ? '' : 'hide'}`} >
+          <li className='navbar-item'><a href="#" className="navbar-link">Services</a></li>
+          <li className='navbar-item'><a href="#" className="navbar-link">Portfolio</a></li>
+          <li className='navbar-item'><a href="#" className="navbar-link">About</a></li>
+          <li className='navbar-item'><a href="#" className="navbar-link">Team</a></li>
+          <li className='navbar-item'><a href="#" className="navbar-link">Contact</a></li>
+        </ul>
+      </nav>
+    )
+  }
 }
